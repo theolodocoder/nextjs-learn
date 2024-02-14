1cc40434e791ec18d93e6c3e3765eafa48263366
theolodocoder <theolodocoder@gmail.com>
2024-02-14 03:35:43 +0100

    ch06 setting vercel/pg db

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,7 +5,9 @@ const {
   revenue,
   users,
 } = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const simplecrypt = require("simplecrypt")
+const sc = simplecrypt()
 
 async function seedUsers(client) {
   try {
@@ -25,7 +27,8 @@ async function seedUsers(client) {
     // Insert data into the "users" table
     const insertedUsers = await Promise.all(
       users.map(async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
+        // const hashedPassword = await bcrypt.hash(user.password, 10);
+        const hashedPassword = await sc.encrypt(user.password)
         return client.sql`
         INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
